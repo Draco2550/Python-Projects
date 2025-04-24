6c72f9623220bcf3c7b20ade09ecd241458ab466
Draco2550 <jednacz25@gmail.com>
2025-04-23 20:48:21 -0400

    Catching up with lessons.

---
+++ Python-Lessons/Lesson1.py
@@ -0,0 +1,11 @@
+#%%
+import numpy as np
+import pandas as pd
+
+
+#%%
+print("This is a test.")
+#%%
+print("Thats crazy!")
+
+# %%
---
+++ Python-Lessons/Lesson2.py
@@ -0,0 +1,22 @@
+#%%
+import numpy as np
+import pandas as pd
+#%%
+Hello = "Goodbye"
+num = 1
+end_num = [1,2,3,4,5]
+for char in Hello:
+    print(char)
+newnum = ""
+for num in end_num:
+     print(end_num)
+
+for num in end_num:
+     print(num)
+# %%
+a = 5
+b = 10
+print(a,b)
+a, b = b,a #This uses tuples to swap the variables without using a temp variable.
+print(a,b)
+# %%
